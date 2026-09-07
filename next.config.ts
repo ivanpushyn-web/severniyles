5f8dcd13cba7d7da6f7b53b539a5772bef124c2e
Ivan Pushyn <ivan.pushyn@gmail.com>
2026-09-07 19:58:49 +0000

    Revert to static export

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  distDir: "dist",
   trailingSlash: true,
-  // SSR режим для Payload CMS
 };
 
 export default nextConfig;
